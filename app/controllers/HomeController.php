3605a836c97777ad5ea1a4aad3ddf52ca3fe646b
Anjali <kambleanjali1993@yahoo.co.in>
2015-08-19 23:31:19 +0530

    J-2 Remove comments from homecontroller

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -32,7 +32,6 @@ class HomeController extends BaseController {
 		);
 
 		return View::make('pages.home', $data);
-		//just for example sake change i am adding this comment here concider it as your original task ok just an example
 	}
 
 }
